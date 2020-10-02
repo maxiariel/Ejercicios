d24fde7235ed49ccff93c0ba9e2a967d6eb45fec
Maximiliano <maximilianoarielvicentin@gmail.com>
2020-10-02 12:48:25 -0300

    Cambio

--- App.js
+++ App.js
@@ -1,1 +1,8 @@
-console.log ('Hello World')
+let caracteres='Hola mundo'
+let cantidad= caracteres.length
+console.log(cantidad)
+
+for(let i=1;i<=3; i++){
+  console.log(caracteres)
+}
+caracteres.substr(0,4)
